c7ccc2a1224771f2fe1196ce4f5e9b4d1217c30f
dsamykov1996 <dsamkov1996@gmail.com>
2025-12-04 15:58:40 +0100

    updated

--- online_store/db.sql
+++ online_store/db.sql
@@ -35,3 +35,64 @@ CREATE TABLE order_item (
 -- Adding items
 INSERT INTO products (name, category, price) 
 VALUES() 
+
+-- CRUD операції для таблиці products
+-- CREATE: додати новий товар
+INSERT INTO products (name, category, price) VALUES ('USB-C Cable', 'Electronics', 12.99);
+-- READ: вибрати всі товари
+SELECT * FROM products;
+-- UPDATE: змінити ціну товару
+UPDATE products SET price = 11.99 WHERE name = 'USB-C Cable';
+-- DELETE: видалити товар
+DELETE FROM products WHERE name = 'USB-C Cable';
+
+
+-- Quantity of orders of all customers
+SELECT c.name, COUNT(o.id) AS orders_count
+FROM customers c
+LEFT JOIN orders o ON o.customer_id = c.identity
+GROUP BY c.id, c.name 
+ORDER BY orders_count DESC;
+
+-- Summary of each order
+SELECT 
+  o.id AS order_id,
+  SUM(oi.quantity * p.price) AS order_total
+FROM orders o
+JOIN order_item oi ON oi.order_id = o.id
+JOIN products1 p ON p.id = oi.product_id
+GROUP BY o.id
+ORDER BY order_total DESC;
+
+-- Top 3 most expensive items
+SELECT name, price
+FROM products
+ORDER BY price DESC
+LIMIT 3;
+
+---------------------
+-- Top 3 most expensive items
+-- SELECT category, name, price
+-- FROM (
+--   SELECT category, name, price,
+--   ROW_NUMBER() OVER 
+--   (PARTITION BY category ORDER BY price DESC) 
+--   AS rank_in_category
+--   FROM products
+-- ) ranked
+-- WHERE rank_in_category <= 3
+-- ORDER BY category, price DESC;
+
+-- Transaction
+
+BEGIN;
+INSERT INTO orders (customer_id) VALUES (2); -- Taras
+INSERT INTO order_item (order_id, product_id, quantity)
+VALUES
+(4, 2, 3),
+(4, 5, 2),
+(4, 4, 1);
+COMMIT;
+
+-- View that shows quantity of orders and customers spents
+
