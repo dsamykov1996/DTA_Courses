877bbb2eff8061e9bdca342710a01d77b5c2bd39
dsamykov1996 <dsamkov1996@gmail.com>
2025-12-05 16:00:07 +0100

    update

---
+++ pandas_5.12.2025_freitag/try.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data = {
+    'product_id': [0, 1],
+    'store1': [95,70],
+    'store2': [100, ""],
+    'store3': [105, 80]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+df_stack = df.set_index('product_id').stack().reset_index()
+
+# df_stack.columns = ['product_id', 'store', 'price']
+
+print(df_stack)
